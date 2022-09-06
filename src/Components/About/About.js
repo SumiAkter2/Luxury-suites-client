172371edd7660b59474d4e10480df0cfca581c36
SumiAkter2 <akteralisha36@gmail.com>
2022-09-06 10:47:57 +0600

    added about compo

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,50 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      {/* about background */}
+      <div
+        className="hero h-56"
+        style={{ backgroundImage: "url(https://i.ibb.co/7gJMvks/room.png)" }}
+      >
+        <div className="hero-overlay bg-opacity-50"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md text-white">
+            <h1 className="mb-5 text-5xl font-serif">About Us</h1>
+            <p className="mb-5 text-xl font-serif">
+              Luxury Suites Like yours own home .
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*  About services */}
+
+      <div className=" text-center py-12">
+        <h1 className=" text-3xl font-serif">Visit Our Famous Facilities</h1>
+        <p className="mb-5 mt-4 font-serif">
+          A wonderful serenity has taken possession of my entire soul, like
+          these sweet mornings of spring which I enjoy with my whole heart.
+        </p>
+      </div>
+
+      {/* restaurant services */}
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img
+            src="https://placeimg.com/260/400/arch"
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 class="text-5xl font-bold">Box Office News!</h1>
+            <p class="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
