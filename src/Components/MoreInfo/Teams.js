09fe710740d4317c3e959fdaf90072271a4d347d
SumiAkter2 <akteralisha36@gmail.com>
2022-09-06 19:57:14 +0600

    added team members

--- src/Components/MoreInfo/Teams.js
+++ src/Components/MoreInfo/Teams.js
@@ -1,12 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { faFacebook, faSoap } from "@fortawesome/free-solid-svg-icons";
+
 const Teams = () => {
   return (
     <div>
       <div
         className="hero h-56"
-        style={{ backgroundImage: "url(https://i.ibb.co/7gJMvks/room.png)" }}
+        style={{ backgroundImage: "url(https://i.ibb.co/9WpnjQD/meeting.jpg)" }}
       >
         <div className="hero-overlay bg-opacity-50"></div>
         <div className="hero-content text-center text-neutral-content">
@@ -14,42 +13,98 @@ const Teams = () => {
             <h1 className="mb-5 text-4xl font-serif">
               Introducing Our Team Members.
             </h1>
+            <p className="text-center flex justify-center items-center cursor-pointer">
+              <span className="mr-2"> {FaceBook}</span>
+              <span className="mr-2"> {twitter}</span>
+              <span className="mr-2"> {instra}</span>
+            </p>
           </div>
         </div>
       </div>
       {/* card for team member */}
-      <div>
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-12 px-12 my-6">
         <div>
-          <img src="https://i.ibb.co/LY9tW3s/man1.jpg" alt="man-img" />
-          <h1>Alisha zaman</h1>
-          <p>{/* <FontAwesomeIcon icon={faFaceBook}></FontAwesomeIcon> */}</p>
+          <img
+            className="h-60  "
+            src="https://i.ibb.co/LY9tW3s/man1.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">Arjun Mollik</h1>
+          <h1 className="text-center ">CEO & Founder</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
+          </p>
         </div>
         <div>
-          <img src="https://i.ibb.co/KhtfYz6/women1.jpg" alt="" />
-          <h1>Alisha zaman</h1>
-          <p>{/* <FontAwesomeIcon icon={faFaceBook}></FontAwesomeIcon> */}</p>
+          <img
+            className="h-60 "
+            src="https://i.ibb.co/KhtfYz6/women1.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">Liesy Suku</h1>
+          <h1 className="text-center ">General Manager</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
+          </p>
         </div>
         <div>
-          <img src="" alt="" />
-          <h1>Alisha zaman</h1>
-          <p>{/* <FontAwesomeIcon icon={faFaceBook}></FontAwesomeIcon> */}</p>
+          <img
+            className="h-60"
+            src="https://i.ibb.co/rHHJGXH/women2.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">kayma Dey</h1>
+          <h1 className="text-center ">Chief Financial Officer</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
+          </p>
         </div>
         <div>
-          <img src="" alt="" />
-          <h1>Alisha zaman</h1>
-          <p>{/* <FontAwesomeIcon icon={faFaceBook}></FontAwesomeIcon> */}</p>
+          <img
+            className="h-60"
+            src="https://i.ibb.co/3RKWRYz/women3.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">Lusi Fansu</h1>
+          <h1 className="text-center ">Chief Technology Officer</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
+          </p>
         </div>
         <div>
-          <img src="" alt="" />
-          <h1>Alisha zaman</h1>
-          <p>{/* <FontAwesomeIcon icon={faFaceBook}></FontAwesomeIcon> */}</p>
+          <img
+            className="h-60"
+            src="https://i.ibb.co/DwHbJFk/man44.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">kiar Roy</h1>
+          <h1 className="text-center ">Hr Manager</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
+          </p>
         </div>
         <div>
-          <img src="" alt="" />
-          <h1>Alisha zaman</h1>
-
-          <p>
-            <FontAwesomeIcon icon={faSoap}></FontAwesomeIcon>
+          <img
+            className="h-60"
+            src="https://i.ibb.co/PWjCVVf/women33.jpg"
+            alt="man-img"
+          />
+          <h1 className="text-center mt-4 text-xl font-bold">Sinai Cor Gill</h1>
+          <h1 className="text-center ">Marking Leader</h1>
+          <p className="text-center flex justify-center items-center cursor-pointer">
+            <span className="mr-2"> {FaceBook}</span>
+            <span className="mr-2"> {twitter}</span>
+            <span className="mr-2"> {instra}</span>
           </p>
         </div>
       </div>
@@ -58,3 +113,33 @@ const Teams = () => {
 };
 
 export default Teams;
+const FaceBook = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    width="20px"
+    fill="orange"
+  >
+    <path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z" />
+  </svg>
+);
+const instra = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    width="20px"
+    fill="orange"
+  >
+    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+  </svg>
+);
+const twitter = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    width="20px"
+    fill="orange"
+  >
+    <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+  </svg>
+);
