f0eca2057784fcca06c09a4af9e1826a111c50e2
SumiAkter2 <akteralisha36@gmail.com>
2023-03-01 09:13:10 +0600

    added booking page

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -3,7 +3,7 @@ import React from "react";
 const Booking = () => {
   return (
     <div>
-      <h1> hhhhh</h1>
+      <h1 className="text-3xl font-bold"> Your Reservation</h1>
     </div>
   );
 };
